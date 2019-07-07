2c0c84f684ca6db17817dca1ee726026545a2e0d
NicolasMCP <nicolas.mcp@gmail.com>
2019-07-07 20:16:20 -0300

    Operador Condicional

---
+++ c2.1.8OperadorCondicional/c2.1.8OperadorCondicional/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+/*
+ * Autor: Nicolas Ramos
+ * 
+ * Ambiente: Visual Studio 2019 C#
+ * Licença: GNU GPL 3
+ * 
+ */
+
+namespace c2._1._8OperadorCondicional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("(Somente use valores numéricos inteiros.)\n\r");
+            Console.Write("Digite o valor de 'a': ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite o valor de 'b': ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            string str = (a > b) ? "'a' É Maior que 'b'" : (a < b) ? "'b' É Maior que 'a'" : "'a' E 'b' são Iguais.";
+
+            Console.WriteLine($"\n\ra = {a}\n\rb = {b}\n\r\n\r" + str);
+        }
+    }
+}
---
+++ c2.1.8OperadorCondicional2/c2.1.8OperadorCondicional2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+/*
+ * Autor: Nicolas Ramos
+ * 
+ * Ambiente: Visual Studio 2019 C#
+ * Licença: GNU GPL 3
+ * 
+ */
+
+namespace c2._1._8OperadorCondicional2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("(Somente use valores numéricos inteiros.)\n\r");
+
+            Console.Write("Digite o valor de 'a': ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite o valor de 'b': ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int c = (a > b) ? a : b;
+
+            Console.WriteLine($"\n\ra = {a}\n\rb = {b}\n\r\n\rO maior valor é {c}");
+        }
+    }
+}
