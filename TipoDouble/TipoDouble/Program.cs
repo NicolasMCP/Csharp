83a250c628a926e73c03a2b29e0679729670d3f0
NicolasMCP <nicolas.mcp@gmail.com>
2019-07-07 19:52:56 -0300

    Variaveis

---
+++ TipoDouble/TipoDouble/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+/*
+ * Autor: Nicolas Ramos
+ * 
+ * Ambiente: Visual Studio 2019 C#
+ * Licença: GNU GPL 3
+ * 
+ */
+namespace TipoDouble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 11;
+            double b = 3;
+            double c = a / b;
+            double max = double.MaxValue;
+            double min = double.MinValue;
+
+            Console.WriteLine($"{a} / {b} = {c}\n\r");
+            Console.WriteLine($"double pode conter valores desde {min} até {max}");
+        }
+    }
+}
