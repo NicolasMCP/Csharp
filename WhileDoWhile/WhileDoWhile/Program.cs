4231b185c5609402c94a83680ebe6c221698eedb
NicolasMCP <nicolas.mcp@gmail.com>
2019-07-09 19:58:39 -0300

    Exemplos de loops

---
+++ WhileDoWhile/WhileDoWhile/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace WhileDoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while(i<=10)
+            {
+                Console.Write((i++) + " ");
+            }
+            Console.WriteLine();
+
+            i--;
+
+            while(i>0)
+            {
+                Console.Write((i--) + " ");
+            }
+            Console.WriteLine();
+
+            i++;
+
+            do
+            {
+                Console.Write((i++) + " ");
+            } while (i <= 10);
+            Console.WriteLine();
+
+            i--;
+
+            do
+            {
+                Console.Write((i--) + " ");
+            } while (i > 0);
+            Console.WriteLine();
+
+            /* Diferença entre while e do while */
+            i = 1;
+
+            Console.WriteLine("While passa ");
+            /* Primeiro comprova a condição, depois se for satizfeita executa o código */
+            while(i > 1)
+            {
+                Console.WriteLine(i++);
+                if (i > 3) break;
+            }
+
+            Console.Write("do While passa ");
+            /* Executa o código primeiro e depóis comprova a condição, se for satizfeita volta a executar */
+            do
+            {
+                Console.Write((i++) + " ");
+                if (i > 3) break;
+            } while (i > 1);
+
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ oFor/oFor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace oFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=1; i<=10; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+
+
+            for(int i=10; i>0; i--)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
