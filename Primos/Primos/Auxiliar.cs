b0c1e093c8ee3aeb0f23ba9462b0c74f11d7e2f9
NicolasMCP <nicolas.mcp@gmail.com>
2019-07-11 16:19:27 -0300

    Números Primos (calcula intervalo)

---
+++ Primos/Primos/Auxiliar.cs
@@ -0,0 +1,60 @@
+﻿/*
+ * Autor: Nicolas Ramos
+ * 
+ * Ambiente: Visual Studio 2019 C#
+ * 
+ * Licença: GNU GPL 3
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Primos
+{
+    class Auxiliar
+    {
+        long[] primos;
+
+        public void Primos(long ini, long fim)
+        {
+            long total = fim / 2;
+            if (total < 10) total = 10;
+            long x = ini;
+            long i = 0;
+            primos = new long[total];
+
+            while (x <= fim)
+            {
+                if ((x == 1) ||
+                    (x == 2) ||
+                    (x == 3))
+                {
+                    primos[i++] = x++;
+                    continue;
+                }
+                else
+                {
+                    int y;
+                    for (y = 2; (y * y) < x; y++)
+                    {
+                        if (x % y == 0) break;
+                    }
+                    if ((y * y) > x) primos[i++] = x;
+                    x++;
+                }
+            }
+        }
+
+        public void Imprime()
+        {
+            if (primos == null) return;
+
+            foreach (long p in primos)
+            {
+                if (p == 0) break;
+                Console.Write($"{p} ");
+            }
+        }
+    }
+}
---
+++ Primos/Primos/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * Autor: Nicolas Ramos
+ * 
+ * Ambiente: Visual Studio 2019 C#
+ * 
+ * Licença: GNU GPL 3
+ * 
+ */
+using System;
+
+namespace Primos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Intervalo de números PRIMOS\n\r");
+            long ini = LerLong("Inicial: ");
+            long fim = LerLong("Final  : ");
+
+            Auxiliar a = new Auxiliar();
+            a.Primos(ini, fim);
+
+            Console.WriteLine("\nNúmeros Primos Resultantes:\n");
+
+            a.Imprime();
+            
+            Console.WriteLine();
+        }
+
+        private static long LerLong(string msg)
+        {
+            Console.Write(msg);
+            return Convert.ToInt64(Console.ReadLine());
+        }
+    }
+}
